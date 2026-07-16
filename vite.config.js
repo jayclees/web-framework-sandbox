95215c789fd8ad01af99260f8384f0925bf81427
jake <jake.lee.web@gmail.com>
2026-07-16 10:54:42 -0700

    add vite support for auto reload on compile, and when assets are updated

--- vite.config.js
+++ vite.config.js
@@ -8,9 +8,6 @@ export default defineConfig({
             origin: 'http://172.19.0.2',
         },
         origin: 'http://172.19.0.2',
-        watch: {
-
-        },
     },
     build: {
         // generate .vite/manifest.json in outDir
@@ -45,9 +42,11 @@ export default defineConfig({
 function watchResourceDir() {
     return {
         name: 'vite-plugin-sturdy-framework',
-        handleHotUpdate(ctx) {
-            if (ctx.file.startsWith(`${__dirname}/resource`)) {
-                ctx.server.ws.send({ type: 'full-reload' })
+        handleHotUpdate({ file, server }) {
+            let pattern = `^${RegExp.escape(__dirname)}\\/target\\/debug\\/[^/]+\\.d$`
+            let regex = new RegExp(pattern)
+            if (file.startsWith(`${__dirname}/resource`) || regex.test(file)) {
+                server.ws.send({ type: 'full-reload' })
             }
 
             return []
