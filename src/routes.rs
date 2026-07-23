3ab51923a4dc8dd62c4066ef7d804b3e1d41a8f7
jake <jake.lee.web@gmail.com>
2026-07-23 00:25:06 -0700

    update welcome page

--- src/routes.rs
+++ src/routes.rs
@@ -2,5 +2,5 @@ use crate::action::pages::Welcome;
 use sturdy::routing::router::Router;
 
 pub fn register_routes(router: &mut Router) -> () {
-    router.getm("/", Welcome::new("Welcome to Sturdy Framework."), |route| route.name("landing"));
+    router.get("/", Welcome::new("Welcome to Sturdy Framework"));
 }
